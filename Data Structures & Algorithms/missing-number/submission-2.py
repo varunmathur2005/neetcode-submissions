bbb7798c369302ba5d3174f65bd59895009bc2f7
Varun Mathur <130909000+varunmathur2005@users.noreply.github.com>
2026-09-16 10:31:56 -0400

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        xor = len(nums)
+        for i in range(len(nums)):
+            xor ^= i
+            xor ^= nums[i]
+        
+        return xor
